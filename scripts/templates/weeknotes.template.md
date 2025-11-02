912153f435491bcd14d1bbcbab24faff46d2f2fe
Suhrid Mulay <suhridmulay@gmail.com>
2025-11-02 09:27:56 +0530

    update weeknotes template

--- scripts/templates/weeknotes.template.md
+++ scripts/templates/weeknotes.template.md
@@ -1,6 +1,7 @@
 ---
-title: Week {{ WEEK }} In the Year of our Lord, 2025
-description: Weeknotes for the {{ WEEK }} Week of the year 2025
+title: Week $WEEK In the Year of our Lord, 2025
+description: Weeknotes for the $WEEK_ORDINAL Week of the year 2025
+published: $EXPECTED_PUBLISH_DATE
 layout: weeknotes.njk
 tags: weeknotes
 ---
--- scripts/weeknotes.sh
+++ scripts/weeknotes.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -e  # exit on error
 
+# we need this because our template directory is defined relative to the script
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 
-YEAR=$(date +%Y)
-WEEK=$(date +%U)
+export YEAR=$(date +%Y)
+export WEEK=$(date +%U)
+
+export WEEK_ORDINAL=$(echo $WEEK | sed -E '
+s/([0-9]*1)$/\1st/
+s/([0-9]*2)$/\1nd/
+s/([0-9]*3)$/\1rd/
+s/([0-9]*[04-9])$/\1th/
+s/([0-9]*1[1-3])$/\1th/
+')
+
+# TODO: verify if this works on linux machines
+export EXPECTED_PUBLISH_DATE=$(date -v +1w -v sun -v 10H -v 0M -v 0S -R)
 BRANCH_NAME="weeknotes-$YEAR-week-$WEEK"
 FILE_PATH="src/posts/weeknotes/${YEAR}-${WEEK}.md"
 
@@ -27,7 +39,8 @@ mkdir -p weeknotes
 # Create the file if it doesn’t exist
 if [ ! -f "$FILE_PATH" ]; then
   echo "[INFO] creating new weeknote: $FILE_PATH"
-  cp "$script_dir/templates/weeknotes.template.md" "$FILE_PATH"
+  TEMPLATE_PATH="$script_dir/templates/weeknotes.template.md"
+  envsubst < $TEMPLATE_PATH > $FILE_PATH
 fi
 
 echo "[INFO] done"
