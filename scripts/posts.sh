bcf1ebe85db23dd89b63af578f310f1bfc6e6374
Suhrid Mulay <suhridmulay@gmail.com>
2026-03-07 17:12:17 +0530

    feat: add script to generate posts from template

---
+++ scripts/posts.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+CURRENT_SCRIPT_FOLDER=$(dirname ${BASH_SOURCE[0]})
+
+# Set up constants and config
+BASE_GIT_BRANCH=main
+POSTS_DIR=src/posts/longforms
+POSTS_TEMPLATE_PATH="$CURRENT_SCRIPT_FOLDER/templates/posts.template.md"
+
+# If the user is on main branch warn the user and exit
+BRANCH=$(git branch --show-current)
+if [[ "$BRANCH" == "$BASE_GIT_BRANCH" ]]; then
+    echo "[WARNING] it is inadvisable to push posts from main."
+    exit 1
+fi
+
+# Check for correct title from user
+TITLE=$1
+if [ -z "$TITLE" ]; then
+  echo "usage $0 [title of the post]"
+  exit 1
+fi
+TITLE_SLUG=$(echo $TITLE | tr -d "'" | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+|-+$//g' | tr "[:upper:]" "[:lower:]")
+
+FILENAME="$TITLE_SLUG.md"
+FILEPATH="$POSTS_DIR/$FILENAME"
+
+if [ ! -f "$FILEPATH" ]; then
+    echo "[INFO] creating post at $FILEPATH"
+    envsubst < $POSTS_TEMPLATE_PATH > $FILEPATH
+fi;
+
+echo "[INFO] done"
---
+++ scripts/templates/posts.template.md
@@ -0,0 +1,8 @@
+---
+title: $POST_TITLE
+description: $DESCRIPTION
+published: $EXPECTED_PUBLISH_DATE
+publishedISO: $EXPECTED_PUBLISH_DATE_ISO
+layout: post.njk
+tags: post
+---
